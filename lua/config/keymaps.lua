a9c4aa686f262d67a2c549f1474430a4d162626f
InNiD <D2862519962@outlook.com>
2026-01-07 21:18:13 +0800

    feat: add noremap to kepmaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,5 @@
 local opts = {
+    noremap = true,
     silent = true,
 }
 --------------
@@ -6,20 +7,20 @@ local opts = {
 --------------
 
 -- Map <A-j>, <A-k>, <A-h>, <A-l> to navigate between windows in any modes
-vim.keymap.set({ "n" }, "<A-h>", "<C-w>h")
-vim.keymap.set({ "n" }, "<A-j>", "<C-w>j")
-vim.keymap.set({ "n" }, "<A-k>", "<C-w>k")
-vim.keymap.set({ "n" }, "<A-l>", "<C-w>l")
-vim.keymap.set({ "t", "i" }, "<A-h>", "<C-\\><C-n><C-w>hi")
-vim.keymap.set({ "t", "i" }, "<A-j>", "<C-\\><C-n><C-w>ji")
-vim.keymap.set({ "t", "i" }, "<A-k>", "<C-\\><C-n><C-w>ki")
-vim.keymap.set({ "t", "i" }, "<A-l>", "<C-\\><C-n><C-w>li")
+vim.keymap.set({ "n" }, "<A-h>", "<C-w>h", opts)
+vim.keymap.set({ "n" }, "<A-j>", "<C-w>j", opts)
+vim.keymap.set({ "n" }, "<A-k>", "<C-w>k", opts)
+vim.keymap.set({ "n" }, "<A-l>", "<C-w>l", opts)
+vim.keymap.set({ "t", "i" }, "<A-h>", "<C-\\><C-n><C-w>hi", opts)
+vim.keymap.set({ "t", "i" }, "<A-j>", "<C-\\><C-n><C-w>ji", opts)
+vim.keymap.set({ "t", "i" }, "<A-k>", "<C-\\><C-n><C-w>ki", opts)
+vim.keymap.set({ "t", "i" }, "<A-l>", "<C-\\><C-n><C-w>li", opts)
 
 -- 分屏和关闭分屏
-vim.keymap.set({ "n", "i", "v", "t"}, "<A-->", "<Cmd>split<CR>")
-vim.keymap.set({ "n", "i", "v", "t"}, "<A-\\>", "<Cmd>vsplit<CR>")
-vim.keymap.set({ "n", "i", "v", "t"}, "<A-w>", "<Cmd>close<CR>")
-vim.keymap.set({ "n", "i", "v", "t"}, "<A-o>", "<Cmd>only<CR>")
+vim.keymap.set({ "n", "i", "v", "t"}, "<A-->", "<Cmd>split<CR>", opts)
+vim.keymap.set({ "n", "i", "v", "t"}, "<A-\\>", "<Cmd>vsplit<CR>", opts)
+vim.keymap.set({ "n", "i", "v", "t"}, "<A-w>", "<Cmd>close<CR>", opts)
+vim.keymap.set({ "n", "i", "v", "t"}, "<A-o>", "<Cmd>only<CR>", opts)
 
 -- Resize with arrows
 -- delta: 2 lines
@@ -33,14 +34,14 @@ vim.keymap.set({ "n", "i", "v", "t"}, "<A-Right>", "<Cmd>vertical resize +2<CR>"
 -----------------
 
 -- 使用x或s删除文本时不将删除内容添加到剪切板
-vim.keymap.set("n", "x", '"_x')
-vim.keymap.set("n", "s", '"_s')
+vim.keymap.set("n", "x", '"_x', opts)
+vim.keymap.set("n", "s", '"_s', opts)
 
 -----------------
 -- INSERT mode --
 -----------------
 
-vim.keymap.set("i", "jk", "<Esc>")
+vim.keymap.set("i", "jk", "<Esc>", opts)
 
 -----------------
 -- Visual mode --
@@ -51,8 +52,8 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", opts)
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", opts)
 
 -- 缩进后保持visual模式而不是变回normal模式
-vim.keymap.set("v", "<", "<gv")
-vim.keymap.set("v", ">", ">gv")
+vim.keymap.set("v", "<", "<gv", opts)
+vim.keymap.set("v", ">", ">gv", opts)
 
 -------------------
 -- Terminal mode --
